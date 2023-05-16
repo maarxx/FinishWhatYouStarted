18044ea94af8c262be82ac10f936ab37f094c731
maarxx <chris.lewark@gmail.com>
2023-05-16 13:26:06 -0400

    Test Custom Bill Subclass With Hello World Label

---
+++ FinishWhatYouStarted/FinishWhatYouStarted_Bill.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using Verse;
+
+namespace FinishWhatYouStarted
+{
+    public class FinishWhatYouStarted_Bill : Bill_Production
+    {
+        public override string Label
+        {
+            get
+            {
+                return "HELLO WORLD!!";
+            }
+        }
+        public FinishWhatYouStarted_Bill()
+        {
+            // Intentionally left blank.
+        }
+    }
+}
--- FinishWhatYouStarted/Patches.cs
+++ FinishWhatYouStarted/Patches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using System.Reflection;
 using Verse;
 
@@ -12,6 +13,24 @@ namespace FinishWhatYouStarted
             var harmony = new Harmony("com.github.harmony.rimworld.maarx.finishwhatyoustarted");
             Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.finishwhatyoustarted");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    // RimWorld.BillUtility
+    // public static Bill MakeNewBill(this RecipeDef recipe, Precept_ThingStyle precept = null)
+    [HarmonyPatch(typeof(BillUtility))]
+    [HarmonyPatch("MakeNewBill")]
+    class Patch_BillUtility_MakeNewBill
+    {
+        static bool Prefix(RecipeDef recipe, Precept_ThingStyle precept, ref Bill __result)
+        {
+            Log.Message("HELLO FROM Patch_BillUtility_MakeNewBill");
+            if (recipe.defName == "FinishWhatYouStarted_Recipe")
+            {
+                __result = new FinishWhatYouStarted_Bill();
+                return false;
+            }
+            return true;
         }
     }
 }
