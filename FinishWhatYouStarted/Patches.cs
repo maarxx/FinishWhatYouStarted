1898a13c50aa948dbc3c915e5400b7995058c40f
maarxx <chris.lewark@gmail.com>
2023-05-16 11:35:07 -0400

    Add Harmony, Trigger Patching

---
+++ FinishWhatYouStarted/Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace FinishWhatYouStarted
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.github.harmony.rimworld.maarx.finishwhatyoustarted");
+            Log.Message("Hello from Harmony in scope: com.github.harmony.rimworld.maarx.finishwhatyoustarted");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
