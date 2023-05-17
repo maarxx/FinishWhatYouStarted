3599662bece0ecc3887b96cc669ebfd590cf2245
maarxx <chris.lewark@gmail.com>
2023-05-17 00:43:30 -0400

    Retain Bill on Workbench Through Save/Load

--- FinishWhatYouStarted/FinishWhatYouStarted_Bill.cs
+++ FinishWhatYouStarted/FinishWhatYouStarted_Bill.cs
@@ -5,6 +5,12 @@ namespace FinishWhatYouStarted
 {
     public class FinishWhatYouStarted_Bill : Bill_Production
     {
+
+        public FinishWhatYouStarted_Bill()
+        {
+            // Intentionally left blank.
+        }
+
         public FinishWhatYouStarted_Bill(RecipeDef recipe, Precept_ThingStyle precept = null) : base(recipe, precept)
         {
             // Intentionally left blank.
